35e46ff465ba9406d071b0cb0a0d768a29a5f60a
Tom-Martin-515 <tommartin2018@outlook.com>
2026-06-24 17:25:36 +0100

    Update root route to use GuestNotesController

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\GuestNoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GuestNotesController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
